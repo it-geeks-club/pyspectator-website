aeef623a32a91cd691a7a41029b6ebf9c08eff56
uzumaxy <uzumaxy@gmail.com>
2014-06-21 23:31:52 +0300

    Code optimization.

--- pyspectator_tornado/static/js/monitor/network.js
+++ pyspectator_tornado/static/js/monitor/network.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    network_info_updater = new NetworkInfoUpdater({
+    var network_info_updater = new NetworkInfoUpdater({
         label_bytes_sent: '#bytes_sent',
         label_bytes_recv: '#bytes_recv',
         interval: 3000
@@ -16,26 +16,38 @@ function NetworkInfoUpdater(params) {
 
     this.label_bytes_sent = $(params.label_bytes_sent);
 
+    this.actual_bytes_sent = null;
+
     this.label_bytes_recv = $(params.label_bytes_recv);
+
+    this.actual_bytes_recv = null;
 
     this.interval = params.interval;
 
     this.start_updating = function() {
         setTimeout(
-            function() { setInterval(self.update, self.interval) },
+            function() { setInterval(self.update, self.interval); },
             self.interval
         );
     }
 
     this.update = function() {
-        $.ajax({
-            url: '/api/computer_info/network_interface.bytes_sent&network_interface.bytes_recv',
-            dataType: 'json',
-            success: function(data) {
-                self.label_bytes_sent.text(data['network_interface.bytes_sent']);
-                self.label_bytes_recv.text(data['network_interface.bytes_recv']);
-            }
-        });
+        $.get(
+            '/api/computer_info/network_interface.bytes_sent&network_interface.bytes_recv',
+            function(data) {
+                var bytes_sent = data['network_interface.bytes_sent'];
+                var bytes_recv = data['network_interface.bytes_recv'];
+                if(self.actual_bytes_sent !== bytes_sent) {
+                    self.actual_bytes_sent = bytes_sent;
+                    self.label_bytes_sent.text(bytes_sent);
+                }
+                if(self.actual_bytes_recv !== bytes_recv) {
+                    self.actual_bytes_recv = bytes_recv;
+                    self.label_bytes_recv.text(bytes_recv);
+                }
+            },
+            'json'
+        );
     }
 
 }
