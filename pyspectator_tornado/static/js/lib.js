1e7aca73644060be5b080b2435b8ae34d7306645
uzumaxy <uzumaxy@gmail.com>
2014-06-24 16:24:39 +0300

    Add string format function.

---
+++ pyspectator_tornado/static/js/lib.js
@@ -0,0 +1,14 @@
+// Replaces one or more format items in a specified string
+// with the string representation of a specified object.
+// Returns formatted string.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
